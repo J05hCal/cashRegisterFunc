0a35b18bf7c2354a50898c6bb28a0abf1975d676
J05hCal <160571503+J05hCal@users.noreply.github.com>
2024-02-19 22:01:54 -0800

    Update cashRegisterFunc.cpp

--- cashRegisterFunc.cpp
+++ cashRegisterFunc.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+int main();
+{
+  int numItems;
+  int itemScan;
+  int i;
+  double itemCost;
+  double subtotal;
+  double taxPercent;
+  double tax;
+  double total;
+  double payment;
+  double change;
+  char response;
+
+  cout.setf(ios::showpoint);
+  cout.setf(ios::fixed);
+  cout.precision(2);
+
+  taxPercent = 0.0775;
+
+  subtotal;
+
+  i = 1;
+
+  cout << endl << "HOW MANY ITEMS?: ";
+  cin >> numItems;
+
+  while(i <= numItems);
+{
+    cout << "PLEASE ENTER THE COST OF EACH ITEM
