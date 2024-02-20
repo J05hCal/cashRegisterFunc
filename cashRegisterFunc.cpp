123d8df086a13ec42e83893e6961833238c68b54
Isaiah714 <Ip714@csu.fullerton.edu>
2024-02-19 22:41:05 -0800

    I fixed some errors.

--- cashRegisterFunc.cpp
+++ cashRegisterFunc.cpp
@@ -1,34 +1,52 @@
 #include <iostream>
+
 #include <cmath>
+
 using namespace std;
 
-int main();
-{
-  int numItems;
-  int itemScan;
-  int i;
-  double itemCost;
-  double subtotal;
-  double taxPercent;
-  double tax;
-  double total;
-  double payment;
-  double change;
-  char response;
+// global variables needed 
+int numItems;
+int itemScan;
+int i;
+double itemCost;
+double subtotal;
+double taxPercent;
+double tax;
+double total;
+double payment;
+double change;
+char response;
 
+/*--------------------------------------MAIN PROGRAM BODY----------------------------------------*/
+
+int main()
+{
+  // Sets the precise decimal points
   cout.setf(ios::showpoint);
   cout.setf(ios::fixed);
   cout.precision(2);
 
+  // Adds the tax percent for the total cost. 
   taxPercent = 0.0775;
 
   subtotal;
 
-  i = 1;
+  i = 0;
 
   cout << endl << "HOW MANY ITEMS?: ";
   cin >> numItems;
 
-  while(i <= numItems);
-{
-    cout << "PLEASE ENTER THE COST OF EACH ITEM
+  
+  while(i < numItems)
+  {
+
+    cout << "PLEASE ENTER THE COST OF EACH ITEM: ";
+
+    cin >> itemCost;
+
+    i++;
+  };
+
+  cout << numItems; 
+    
+  }
